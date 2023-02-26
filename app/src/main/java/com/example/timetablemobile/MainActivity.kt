05e5890b2f840fcca1ad8267c0031bf527cbd57e
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2023-02-26 17:46:54 +0700

    Баг с загрузкой экрана пофикшен

--- app/src/main/java/com/example/timetablemobile/MainActivity.kt
+++ app/src/main/java/com/example/timetablemobile/MainActivity.kt
@@ -6,14 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.timetablemobile.ui.presentation.signinscreen.SignInScreen
 import com.example.timetablemobile.ui.theme.TimetableMobileTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             TimetableMobileTheme {
-                SignInScreen()
             }
         }
     }
--- app/src/main/java/com/example/timetablemobile/di/AppModule.kt
+++ app/src/main/java/com/example/timetablemobile/di/AppModule.kt
@@ -4,31 +4,24 @@ import com.example.timetablemobile.common.Constants
 import com.example.timetablemobile.data.remote.AuthApi
 import com.example.timetablemobile.data.repository.AuthRepositoryImpl
 import com.example.timetablemobile.domain.repository.AuthRepository
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    private val json = Json {
-        ignoreUnknownKeys = true
-        coerceInputValues = true
-    }
-
     @Provides
     @Singleton
     fun provideAuthApi(): AuthApi{
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(AuthApi::class.java)
     }
