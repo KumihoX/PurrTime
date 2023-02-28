cef5966acad96db80f1c5b665b68125d4abb6f6b
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2023-03-01 01:31:00 +0700

    Полностью сверстан главный экран
    Реализована панель недели со скроллом.
    Изменение месяца при переходе между неделями.
    Кнопочки есть, но функционал пока отсутствует

--- app/src/main/java/com/example/timetablemobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/timetablemobile/ui/theme/Color.kt
@@ -8,6 +8,7 @@ val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 val Red = Color(0xFFAD182D)
 val Gray = Color(0xFF696969)
+val LightGray = Color(0xFFECECEC)
 
 val MainGreen = Color(0xFF00615D)
 
