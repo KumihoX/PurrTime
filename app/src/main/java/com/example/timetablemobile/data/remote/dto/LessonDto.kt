ba142709a7d916b3554648ec33e073f2d1f94071
Solntselikaya <solntselikaya.sofia@yandex.ru>
2023-03-09 00:15:34 +0700

    Апдейт в LessonDto

--- app/src/main/java/com/example/timetablemobile/data/remote/dto/LessonDto.kt
+++ app/src/main/java/com/example/timetablemobile/data/remote/dto/LessonDto.kt
@@ -1,5 +1,8 @@
 package com.example.timetablemobile.data.remote.dto
 
+import com.example.timetablemobile.domain.model.Lesson
+import com.example.timetablemobile.domain.model.LessonType
+
 data class LessonDto(
     val type: Int,
     val subject: String,
@@ -9,3 +12,39 @@ data class LessonDto(
     val groupsNum: List<Int>,
     val data: String
 )
+
+fun LessonDto.toLesson(): Lesson {
+
+    val lessonType = when(type) {
+        0 -> LessonType.Lection
+        1 -> LessonType.Seminar
+        2 -> LessonType.Practice
+        3 -> LessonType.Lab
+        4 -> LessonType.IndividualLesson
+        5 -> LessonType.ControlWork
+        6 -> LessonType.Consultation
+        7 -> LessonType.Booking
+        else -> LessonType.ContactWork
+    }
+
+    val parsedStart = timeslot.startsAt.substringAfter("T").split(":")
+    val starts = parsedStart[0] + ":" + parsedStart[1]
+
+    val parsedEnd = timeslot.endsAt.substringAfter("T").split(":")
+    val ends = parsedEnd[0] + ":" + parsedEnd[1]
+
+    var groups = ""
+    for(group in groupsNum) {
+        groups = "$groups, $group"
+    }
+
+    return Lesson(
+        type = lessonType,
+        subject = subject,
+        cabinetName = cabinet.number.toString() + " " + cabinet.name,
+        teacher = teacher,
+        startsAt = starts,
+        endsAt = ends,
+        groups = groups
+    )
+}
--- app/src/main/java/com/example/timetablemobile/domain/model/Lesson.kt
+++ app/src/main/java/com/example/timetablemobile/domain/model/Lesson.kt
@@ -1,5 +1,11 @@
 package com.example.timetablemobile.domain.model
 
 data class Lesson(
-    val type: LessonType
+    val type: LessonType,
+    val subject: String,
+    val cabinetName: String,
+    val teacher: String,
+    val startsAt: String,
+    val endsAt: String,
+    val groups: String
 )
